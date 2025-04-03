00c4305ac8161ba1bdf2287a0eef4781d1463539
justeozan <justeoz.an+projects@gmail.com>
2025-04-03 10:00:55 +0200

    Add color output and improve test case for easyfind function

--- C++ Module 08/ex00/easyfind.hpp
+++ C++ Module 08/ex00/easyfind.hpp
@@ -5,12 +5,17 @@
 #include <vector>
 #include <algorithm>
 
+#define BOLD "\033[1m"
+#define GREEN "\033[32m"
+#define RED "\033[31m"
+#define RESET "\033[0m"
+
 template <typename T>
 typename T::iterator easyfind(T &container, int n)
 {
     typename T::iterator it = std::find(container.begin(), container.end(), n);
     if (it == container.end())
-        throw std::runtime_error("Element not found");
+        throw std::runtime_error(RED"Element not found"RESET);
     return it;
 }
 
--- C++ Module 08/ex00/main.cpp
+++ C++ Module 08/ex00/main.cpp
@@ -1,8 +1,22 @@
 #include "easyfind.hpp"
 
 int main( void ) {
-    std::vector<int> vec = {1, 2, 3, 4, 5};
-    auto it = easyfind(vec, 3);
-    std::cout << *it << std::endl;
-    return 0;
+	std::vector<int> vec;
+	for(int i = 0; i < 5; i++)
+		vec.push_back(i * 10);
+	std::cout << "Vector: ";
+	for (int i = 0; i < 5; i++)
+		std::cout << vec[i] << ' ';
+	std::cout << "\n" << std::endl;
+	try {
+		std::cout << BOLD << "Search 30: " << RESET << std::endl;
+		std::vector<int>::iterator it = easyfind(vec, 30);
+		std::cout << GREEN << *it << RESET << std::endl;
+		std::cout << BOLD << "Search 35: " << RESET << std::endl;
+		it = easyfind(vec, 35);
+		std::cout << GREEN << *it << RESET << std::endl;
+	} catch (const std::exception& e) {
+		std::cerr << RED << e.what() << RESET << std::endl;
+	}
+	return 0;
 }
